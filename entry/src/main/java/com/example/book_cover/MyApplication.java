29c40b5317afe8057a341acbddaaf92f826ab876
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-12-17 22:01:00 +0530

    Initialize the library

---
+++ entry/src/main/java/com/example/book_cover/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.book_cover;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
