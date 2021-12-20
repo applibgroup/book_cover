4cb41e33a81d552d910a154cdaa621775d7e6690
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-12-20 15:19:04 +0530

    pass rating as prop

--- BookCover/Card/index.js
+++ BookCover/Card/index.js
@@ -3,13 +3,16 @@ import prompt from '@system.prompt';
 export default {
     data: {
         container: "",
-        datetimevalue:"",
+        display1: false,
+        display2: false,
+        display3: false,
+        display4: false,
+        display5: false,
         imageClass: '',
         title: "",
         subTitle: "",
         textContainer: "",
         bottomContainer: "",
-        pickerdatetime: "",
         dateValue: "",
         description: "",
         topContainer: "",
@@ -26,12 +29,45 @@ export default {
         sub_title: "",
         image: "",
         desc:"",
+        rating: "",
         likes:"",
         views: "",
         btncolor:"",
     },
     onInit() {
+
         if(this.type==="book-card"){
+            if(this.rating == 1){
+                this.display1 = true;
+                this.display2 = false;
+                this.display3 = false;
+                this.display4 = false;
+                this.display5 = false;
+            } else if(this.rating == 2){
+                this.display1 = true;
+                this.display2 = true;
+                this.display3 = false;
+                this.display4 = false;
+                this.display5 = false;
+            }else if(this.rating == 3){
+                this.display1 = true;
+                this.display2 = true;
+                this.display3 = true;
+                this.display4 = false;
+                this.display5 = false;
+            }else if(this.rating == 4){
+                this.display1 = true;
+                this.display2 = true;
+                this.display3 = true;
+                this.display4 = true;
+                this.display5 = false;
+            }else if(this.rating == 5){
+                this.display1 = true;
+                this.display2 = true;
+                this.display3 = true;
+                this.display4 = true;
+                this.display5 = true;
+            }
             this.container="book-card";
             this.imageClass="image";
             this.starContainer="starContainer";
@@ -40,23 +76,10 @@ export default {
             this.subTitle= "spec";
             this.textContainer="textContainer";
             this.bottomContainer="bottomContainer";
-            this.pickerdatetime="pickerdatetime";
             this.dateValue="text";
             this.topContainer="topContainer";
             this.bottomTextContainer="bottomTextContainer";
-            this.valueContainer="valueContainerBlock";
-            this.readMore="readMoreBlock";
         }
-    },
-
-    changestar(e) {
-          var x = document.getElementById("stars");
-          document.getElementById("stars");
-          if (x.style.display === "none") {
-              x.style.display = "block";
-          } else {
-              x.style.display = "none";
-          }
     },
 }
 
